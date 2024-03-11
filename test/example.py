45c2f130f0413231c244d5fabe038aef4b8e87bf
Pierre Gueth <gueth@adobe.com>
2024-03-11 16:52:37 +0100

    on the way

--- test/example.py
+++ test/example.py
@@ -5,6 +5,7 @@ import trimesh
 from diso import DiffMC
 from diso import DiffDMC
 
+
 # define a sphere SDF
 class SphereSDF:
     def __init__(self, center, radius):
@@ -16,14 +17,43 @@ class SphereSDF:
         return torch.norm(points - self.center, dim=-1) - self.radius
 
 
+class CapsuleSDF:
+    def __init__(self, center, radius, height):
+        self.center = center
+        self.radius = radius
+        self.half_height = height / 2
+        delta = torch.tensor([self.radius, self.radius, self.half_height + self.radius])
+        self.aabb = torch.stack([center - delta, center + delta], dim=-1)
+
+    def __call__(self, points_):
+        points = torch.abs(points_ - self.center)
+        # xys = points[:, :, :, :2]
+        # zzs = points[:, :, :, 2]
+        # selection = zzs > self.height
+        # print("$$$", selection.shape, selection.dtype, xys.shape, zzs.shape)
+        dists = torch.norm(points - torch.tensor([0, 0, self.half_height]), dim=-1)
+        # dists = (
+        #     torch.select(
+        #         selection,
+        #         torch.norm(xys, dim=-1),
+        #         torch.norm(points - torch.tensor([0, 0, self.height]), dim=-1),
+        #     )
+        #     - self.radius
+        # )
+        print("!!!", points.shape, dists.shape)
+        return dists - self.radius
+
+
 os.makedirs("out", exist_ok=True)
 
 # define a sphere
-s_x = 0.
-s_y = 0.
-s_z = 0.
+s_x = 0.0
+s_y = 0.0
+s_z = 0.0
 radius = 0.5
-sphere = SphereSDF(torch.tensor([s_x, s_y, s_z]), radius)
+height = 1.5
+# sphere = SphereSDF(torch.tensor([s_x, s_y, s_z]), radius)
+shape = CapsuleSDF(torch.tensor([s_x, s_y, s_z]), radius, height)
 
 # create the iso-surface extractor
 diffmc = DiffMC(dtype=torch.float32).cuda()
@@ -40,18 +70,12 @@ grids = torch.stack(
     ),
     dim=-1,
 )
-grids[..., 0] = (
-    grids[..., 0] * (sphere.aabb[0, 1] - sphere.aabb[0, 0]) + sphere.aabb[0, 0]
-)
-grids[..., 1] = (
-    grids[..., 1] * (sphere.aabb[1, 1] - sphere.aabb[1, 0]) + sphere.aabb[1, 0]
-)
-grids[..., 2] = (
-    grids[..., 2] * (sphere.aabb[2, 1] - sphere.aabb[2, 0]) + sphere.aabb[2, 0]
-)
+grids[..., 0] = grids[..., 0] * (shape.aabb[0, 1] - shape.aabb[0, 0]) + shape.aabb[0, 0]
+grids[..., 1] = grids[..., 1] * (shape.aabb[1, 1] - shape.aabb[1, 0]) + shape.aabb[1, 0]
+grids[..., 2] = grids[..., 2] * (shape.aabb[2, 1] - shape.aabb[2, 0]) + shape.aabb[2, 0]
 
 # query the SDF input
-sdf = sphere(grids)
+sdf = shape(grids)
 sdf = sdf.requires_grad_(True).cuda()
 sdf = torch.nn.Parameter(sdf.clone().detach(), requires_grad=True)
 
@@ -67,26 +91,42 @@ deform = torch.nn.Parameter(
 
 # DiffMC with random grid deformation
 verts, faces = diffmc(sdf, 0.5 * torch.tanh(deform))
-verts = verts.cpu() * (sphere.aabb[:, 1] - sphere.aabb[:, 0]) + sphere.aabb[:, 0]
-mesh = trimesh.Trimesh(vertices=verts.detach().cpu().numpy(), faces=faces.cpu().numpy(), process=False)
+verts = verts.cpu() * (shape.aabb[:, 1] - shape.aabb[:, 0]) + shape.aabb[:, 0]
+mesh = trimesh.Trimesh(
+    vertices=verts.detach().cpu().numpy(),
+    faces=faces.cpu().numpy(),
+    process=False,
+)
 mesh.export("out/diffmc_sphere_w_deform.obj")
 
 # DiffMC without grid deformation
 verts, faces = diffmc(sdf, None)
-verts = verts.cpu() * (sphere.aabb[:, 1] - sphere.aabb[:, 0]) + sphere.aabb[:, 0]
-mesh = trimesh.Trimesh(vertices=verts.detach().cpu().numpy(), faces=faces.cpu().numpy(), process=False)
+verts = verts.cpu() * (shape.aabb[:, 1] - shape.aabb[:, 0]) + shape.aabb[:, 0]
+mesh = trimesh.Trimesh(
+    vertices=verts.detach().cpu().numpy(),
+    faces=faces.cpu().numpy(),
+    process=False,
+)
 mesh.export("out/diffmc_sphere_wo_deform.obj")
 
 # DiffDMC with random grid deformation
 verts, faces = diffdmc(sdf, 0.5 * torch.tanh(deform))
-verts = verts.cpu() * (sphere.aabb[:, 1] - sphere.aabb[:, 0]) + sphere.aabb[:, 0]
-mesh = trimesh.Trimesh(vertices=verts.detach().cpu().numpy(), faces=faces.cpu().numpy(), process=False)
+verts = verts.cpu() * (shape.aabb[:, 1] - shape.aabb[:, 0]) + shape.aabb[:, 0]
+mesh = trimesh.Trimesh(
+    vertices=verts.detach().cpu().numpy(),
+    faces=faces.cpu().numpy(),
+    process=False,
+)
 mesh.export("out/diffdmc_sphere_w_deform.obj")
 
 # DiffDMC without grid deformation
 verts, faces = diffdmc(sdf, None)
-verts = verts.cpu() * (sphere.aabb[:, 1] - sphere.aabb[:, 0]) + sphere.aabb[:, 0]
-mesh = trimesh.Trimesh(vertices=verts.detach().cpu().numpy(), faces=faces.cpu().numpy(), process=False)
+verts = verts.cpu() * (shape.aabb[:, 1] - shape.aabb[:, 0]) + shape.aabb[:, 0]
+mesh = trimesh.Trimesh(
+    vertices=verts.detach().cpu().numpy(),
+    faces=faces.cpu().numpy(),
+    process=False,
+)
 mesh.export("out/diffdmc_sphere_wo_deform.obj")
 
 print("examples saved to out/")
